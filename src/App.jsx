e4102ca9976daa1ae5c064d0d144cabe7d0ab9c5
acedamin325 <daminkhan325@gmail.com>
2024-06-15 18:29:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Header from './components/Header'
+
+export default function App() {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    )
+}
+
