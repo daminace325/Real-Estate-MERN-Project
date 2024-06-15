7d9601e4a75244331ad88a4923c7c40397339baf
acedamin325 <daminkhan325@gmail.com>
2024-06-15 18:35:19 +0530

    Home name changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
                 </form>
                 <ul className='flex gap-4'>
                     <Link to='/'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home is home</li>
+                        <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
                     </Link>
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
