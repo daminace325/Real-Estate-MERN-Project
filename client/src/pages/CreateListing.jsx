01a6fdb4fd5c447784e2f624f9a78d5a8fc46979
acedamin325 <daminkhan325@gmail.com>
2024-07-29 21:19:36 +0530

    Textarea update in description (minor change)

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -9,7 +9,7 @@ export default function CreateListing() {
             <form className='flex flex-col sm:flex-row gap-6'>
                 <div className='flex flex-col gap-4 flex-1'>
                     <input type="text" placeholder='Name' id='name' maxLength='62' minLength='10' className='border p-3 rounded-lg' required />
-                    <input type="text" placeholder='Description' id='description' className='border p-3 rounded-lg' required />
+                    <textarea type="text" placeholder='Description' id='description' className='border p-3 rounded-lg' required />
                     <input type="text" placeholder='Address' id='address' className='border p-3 rounded-lg' required />
                     <div className='flex gap-6 flex-wrap'>
                         <div className='flex gap-2'>
