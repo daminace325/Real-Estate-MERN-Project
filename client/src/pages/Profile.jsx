0ec2259df34668491eb203d86caeca93625541a6
acedamin325 <daminkhan325@gmail.com>
2024-08-09 18:12:15 +0530

    Link fixed in Listing in Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -195,10 +195,10 @@ export default function Profile() {
 				<div className='flex flex-col gap-4'>
 					<h1 className='text-center text-2xl font-semibold'>Your Listings</h1>
 					{userListings.map((listing) => <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
-						<Link to={`/listing${listing._id}`}>
+						<Link to={`/listing/${listing._id}`}>
 							<img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain' />
 						</Link>
-						<Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing${listing._id}`}>
+						<Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
 							<p>{listing.name}</p>
 						</Link>
 						<div className='flex flex-col items-center'>
